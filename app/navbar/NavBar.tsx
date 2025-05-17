773759b44a685be9c4bf78b23bf62b1eb99f9e32
Poulastaa Das <poulastaadas2@gmail.com>
2025-05-17 14:36:38 +0530

    fixed resume link

--- app/navbar/NavBar.tsx
+++ app/navbar/NavBar.tsx
@@ -20,7 +20,7 @@ const NavBar = () => {
   return (
     <nav className="fixed bottom-10 left-0 right-0 z-50 my-0  mx-auto  flex w-[306px] items-center justify-center gap-1 rounded-lg bg-[#07070a]/90 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
       <Link
-        href="https://drive.usercontent.google.com/u/0/uc?id=1z1AhLxmFy5n5pY9Adr9W8_pson_HaryK&export=download"
+        href="https://drive.google.com/drive/folders/1oFbXSljyfRTPt-2S10Pifn4okaDyicyL"
         target="_blank"
         className="flex"
         aria-label="Open my resume"
